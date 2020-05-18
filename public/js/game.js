fc5d23b1a261eebc3f8c051b08de43500cc49ff8
a3rtgm-ds <daniel.schulz@hcu-hamburg.de>
2020-05-18 23:55:53 +0200

    move tokens by V

--- public/js/game.js
+++ public/js/game.js
@@ -11,24 +11,31 @@ const Game = () => {
                 data: () => ({
                     board: null,
                     interval: null,
-                    tokens: []
+                    tokens: [],
+                    vel: [0,0],
+                    tar: {
+                        x: 10,
+                        y: 120
+                    },
+                    velocity: 1
                 }),
                 computed: {
                     canvas() {
                         return this.$refs?.board;
                     },
                     ctx() {
-                        return this.board?.getContext("2d")
+                        return this.canvas?.getContext("2d")
                     }
                 },
                 created() {
                     this.listen();
                 },
                 mounted() {
-                    this.canvas.width = window.innerWidth;
-                    this.canvas.height = window.height;
+                    this.canvas.width = window.innerWidth * 0.8;
+                    this.canvas.height = window.innerHeight * 0.6;
+                    this.startInterval();
 
-                    this.startInterval()
+                    this.startGame();
                 },
                 watch: {
                     board() {
@@ -37,13 +44,48 @@ const Game = () => {
                 },
                 methods: {
                     startGame() {
-                        tokens.push(new Token(30, 30, "red", 10, 120, this.ctx));
+                        this.tokens.push(new Token(30, 30, "red", 10, 120, this.ctx));
                     },
                     listen() {
                         this.socket.on('gameState', this.updateBoard);
+
+                        // window.addEventListener("keydown", (evt) => {
+                        //     console.log(evt.code);
+                        //     switch(evt.code) {
+                        //         case "ArrowUp": 
+                        //             this.vel[1] = -2;
+                        //             break;
+                        //         case "ArrowDown": 
+                        //             this.vel[1] = 2;
+                        //             break;
+                        //         case "ArrowLeft": 
+                        //             this.vel[0] = -2;
+                        //             break;
+                        //         case "ArrowRight": 
+                        //             this.vel[0] = 2;
+                        //             break;
+                        //     }
+                        //     console.log(this.vel);
+                        // });
+                        // window.addEventListener("keyup", (evt) => {
+                        //     switch(evt.code) {
+                        //         case "ArrowUp": 
+                        //             this.vel[1] = 0;
+                        //             break;
+                        //         case "ArrowDown": 
+                        //             this.vel[1] = 0;
+                        //             break;
+                        //         case "ArrowLeft": 
+                        //             this.vel[0] = 0;
+                        //             break;
+                        //         case "ArrowRight": 
+                        //             this.vel[0] = 0;
+                        //             break;
+                        //     }
+                        // });
                     },
                     startInterval() {
-                        this.interval = setInterval(this.updateBoard, 20);
+                        this.interval = setInterval(this.updateCanvas, 20);
                     },
                     clearInterval() {
                         clearInterval(this.interval);
@@ -56,6 +98,23 @@ const Game = () => {
                     },
                     updateCanvas() {
                         this.clearCanvas();
+                        this.tokens.forEach(t => {
+                            const V = this.getV(t.x, t.y, this.tar, this.velocity);
+                            t.y += V.y;
+                            t.x += V.x;
+                        });
+                        this.tokens.forEach(t => t.update());
+                    },
+                    getV(posX, posY, tar, vel = 1) {
+                        const dx = tar.x - posX,
+                            dy = tar.y - posY,
+                            v = (Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))) / vel;
+                        
+                        return {
+                            x: dx / v || 0,
+                            y: dy / v || 0
+                        };
+                        
                     }
                 },
                 template: template
@@ -69,11 +128,12 @@ class Token {
         this.height = height;
         this.x = x;
         this.y = y;
+        this.color = color;
         this.ctx = ctx;
     }
 
     update() {
-        this.ctx.fillStyle = color;
+        this.ctx.fillStyle = this.color;
         this.ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 };
